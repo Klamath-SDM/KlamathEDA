67c976f2da4864ee58dbebf0f93a59054cf9bc6d
Maddee Rubenson <mrubenson@flowwest.com>
2023-08-24 16:08:31 -0700

    updating UI to include tabs for other data

--- shiny/klamath_sdm_data_catalog/ui.R
+++ shiny/klamath_sdm_data_catalog/ui.R
@@ -5,35 +5,43 @@ fluidPage(
   theme = bs_theme(
     bootswatch = "yeti"),
   
-  titlePanel("Klamath SDM - Restoration Project Catalog"),
-  h6("The following restoration projects were aggregated through literature review.
+  titlePanel("Klamath SDM - Data Catalog"),
+  
+  tabsetPanel(
+    tabPanel("Restoration Data",
+             titlePanel("Restoration Project Catalog"),
+             h6("The following restoration projects were aggregated through literature review.
            This dataset is not complete and will be updated as watershed-specific stakeholders
            are engaged in the Klamath SDM process."),
-  
-  # Sidebar layout
-  sidebarLayout(
-    sidebarPanel(style = "background-color: #faf9f7;",
-                 h4("Filter by Watershed", style = "color: black;"),
-                 selectInput("watershed", "", choices = c('All Watersheds', unique(hucs$name))),
-                 actionButton("apply_filter", "Apply Filter"),
-                 br(),
-                 hr(),
-                 downloadButton("downloadData", "Download")
-                 
-    ),
-    mainPanel( 
-      leafletOutput('map')
-    )
-  ),
-  hr(),
-  br(),
-  fluidRow(
-    column(12,
-           div(style = "height: 400px; overflow-y: scroll;",
-               dataTableOutput("rest_table")
-           )
-    )
+             sidebarLayout(
+               sidebarPanel(style = "background-color: #faf9f7;",
+                            h4("Filter by Watershed", style = "color: black;"),
+                            selectInput("watershed", "", choices = c('All Watersheds', unique(hucs$name))),
+                            actionButton("apply_filter", "Apply Filter"),
+                            br(),
+                            hr(),
+                            downloadButton("downloadData", "Download")
+                            
+               ),
+               mainPanel( 
+                 leafletOutput('map')
+               )
+             ),
+             hr(),
+             br(),
+             fluidRow(
+               column(12,
+                      div(style = "height: 400px; overflow-y: scroll;",
+                          dataTableOutput("rest_table")
+                      )
+               )
+             )
+    ), 
+    tabPanel("Fisheries Data",
+             #PLACEHOLDER
+             ),
+    tabPanel("Habitat Data",
+             #PLACEHOLDER
+             )
   )
 )
-
-
